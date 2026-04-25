172c691b560c8ef6ef9ad2a5a352715c85f56a33
hopejimmy <hopejimmy@gmail.com>
2026-04-24 19:08:47 -0500

    fix(landing): remove white shadow from MBTI cloud bubble

--- src/components/OnboardingForm.tsx
+++ src/components/OnboardingForm.tsx
@@ -140,15 +140,6 @@ export default function OnboardingForm() {
         >
           <div style={{ position: 'relative', width: 140, height: 116 }}>
             <svg viewBox="0 0 125 100" width="140" height="116" style={{ display: 'block' }}>
-              {/* White offset shadow */}
-              <path
-                transform="translate(5,5)"
-                d="M95,68 Q106,68 112,58 Q120,48 112,38 Q112,22 96,18 Q94,8 80,10 Q72,2 60,10 Q52,2 44,10 Q32,4 26,16 Q14,16 12,28 Q4,36 8,48 Q6,62 18,68 Q28,78 46,70 Q56,76 66,70 L72,86 L95,68 Z"
-                fill="white"
-                stroke="#2A1F1A"
-                strokeWidth="3"
-                strokeLinejoin="round"
-              />
               {/* Main cloud bubble */}
               <path
                 d="M95,68 Q106,68 112,58 Q120,48 112,38 Q112,22 96,18 Q94,8 80,10 Q72,2 60,10 Q52,2 44,10 Q32,4 26,16 Q14,16 12,28 Q4,36 8,48 Q6,62 18,68 Q28,78 46,70 Q56,76 66,70 L72,86 L95,68 Z"
